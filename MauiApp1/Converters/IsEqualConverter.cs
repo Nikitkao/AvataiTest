02d0b7ce01ccfe389d24acda02cae5a3ecd6b684
Nikita <nnn93@mail.ru>
2025-07-24 09:04:42 +0300

    Page 3 finished

---
+++ MauiApp1/Converters/IsEqualConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace MauiApp1.Converters
+{
+    public class IsEqualConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.Equals(parameter) ?? parameter == null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isChecked && isChecked)
+                return parameter;
+
+            return Binding.DoNothing;
+        }
+    }
+}
--- MauiApp1/ViewModels/Page3ViewModel.cs
+++ MauiApp1/ViewModels/Page3ViewModel.cs
@@ -1,20 +1,17 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 
 namespace MauiApp1.ViewModels
 {
     public partial class Page3ViewModel : ObservableObject
     {
-        private readonly Dictionary<string, string> _urlMap = new()
+        private readonly Dictionary<string, string> _optionUrlMap = new()
         {
             { "Основной", "https://avatai.my/" },
             { "Видео", "https://video.avatai.my/" }
         };
 
-        public IEnumerable<string> UrlLabels => _urlMap.Keys;
-
         [ObservableProperty]
-        private string selectedLabel;
+        private string selectedOption;
 
         [ObservableProperty]
         private string currentUrl;
@@ -24,19 +21,21 @@ namespace MauiApp1.ViewModels
 
         public Page3ViewModel()
         {
-            SelectedLabel = "Основной";
-            CurrentUrl = _urlMap[SelectedLabel];
+            SelectedOption = _optionUrlMap.First().Key;
             IsLoading = true;
         }
 
-        //partial void OnSelectedLabelChanged(string oldValue, string newValue)
-        //{
-        //    if (_urlMap.TryGetValue(newValue, out var newUrl) && newUrl != CurrentUrl)
-        //    {
-        //        IsLoading = true;
-        //        CurrentUrl = newUrl;
-        //    }
-        //}
+        partial void OnSelectedOptionChanged(string oldValue, string newValue)
+        {
+            if (newValue is null)
+                return;
+
+            if (_optionUrlMap.TryGetValue(newValue, out var newUrl) && newUrl != CurrentUrl)
+            {
+                IsLoading = true;
+                CurrentUrl = newUrl;
+            }
+        }
 
         public void OnPageLoaded() => IsLoading = false;
     }
